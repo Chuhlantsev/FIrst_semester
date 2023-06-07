98bf0b94a877bb280ca5c706efb0b401151eed4f
Chuhlantsev <Dezmont96@yandex.ru>
2023-06-08 01:43:22 +0500

    LR 6

---
+++ src/lr8/example2/ByteStreamExample_console.java
@@ -0,0 +1,39 @@
+package lr8.example2;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ByteStreamExample_console {
+    public static void main(String[] args) {
+        // Запись данных в файл
+        String fileName = "src/lr8/example2/example_file.txt";
+        Scanner in = new Scanner(System.in);
+        String data = in.nextLine();
+        try (FileOutputStream outputStream = new FileOutputStream(fileName)) {
+            outputStream.write(data.getBytes());
+            System.out.println("Данные записаны в файл: " + fileName);
+        } catch (IOException e) {
+            System.out.println("Ошибка записи в файл: " + e.getMessage());
+        }
+
+        //Чтение данных из файла
+        try (FileInputStream inputStream = new FileInputStream(fileName)) {
+            byte[] buffer = new byte[1024];
+            int bytesRead = inputStream.read(buffer);
+            String readData = new String(buffer, 0, bytesRead);
+            System.out.println("Прочитанные данные: " + readData);
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла: " + e.getMessage());
+        }
+
+        //Удаление файла
+        if (new File(fileName).delete()) {
+            System.out.println("Файл удален: " + fileName);
+        } else {
+            System.out.println("Не удалось отправить файл: " + fileName);
+        }
+    }
+}
---
+++ src/lr8/example3/CharStreamExample_console.java
@@ -0,0 +1,39 @@
+package lr8.example3;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CharStreamExample_console {
+    public static void main(String[] args) {
+        //Запись данных в файл
+        String fileName = "src/lr8/example3/example_file.txt";
+        Scanner in = new Scanner(System.in);
+        String data = in.nextLine();
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write(data);
+            System.out.println("Данные записаны в файл: " + fileName);
+        } catch (IOException e) {
+            System.out.println("Ошибка записи в файл: " + e.getMessage());
+        }
+
+        //Чтение данных аз файла
+        try (FileReader reader = new FileReader(fileName)) {
+            char[] buffer = new char[1024];
+            int charRead = reader.read(buffer);
+            String readData = new String(buffer, 0, charRead);
+            System.out.println("Прочитанные данные: " + readData);
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла: " + e.getMessage());
+        }
+
+        //Удаление файла
+        if (new File(fileName).delete()) {
+            System.out.println("Файл удален: " + fileName);
+        } else {
+            System.out.println("Не удалось отправить файл: " + fileName);
+        }
+    }
+}
